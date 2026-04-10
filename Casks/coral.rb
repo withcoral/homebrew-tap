d69d5596a98f068b864d0ced80164480233285d9
Simon Whitaker <simonw@withcoral.com>
2026-04-10 07:43:48 +0100

    chore: disable completion generation

--- Casks/coral.rb
+++ Casks/coral.rb
@@ -10,8 +10,8 @@ cask "coral" do
   end
 
   binary "coral"
-  generate_completions_from_executable "coral",
-    shell_parameter_format: :clap
+  # generate_completions_from_executable "coral",
+  #   shell_parameter_format: :clap
 
 
   on_macos do
