9011cac3848452d8815f0d9a399419a80f9e095d
Simon Whitaker <simonw@withcoral.com>
2026-04-27 15:09:58 +0100

    chore: enable completion generation

--- Casks/coral.rb
+++ Casks/coral.rb
@@ -11,8 +11,7 @@ cask "coral" do
 
   binary "coral"
 
-  # generate_completions_from_executable "coral",
-  #   shell_parameter_format: :clap
+  generate_completions_from_executable("coral", "completion", shells: [:bash, :zsh, :fish])
 
   on_macos do
     on_intel do
